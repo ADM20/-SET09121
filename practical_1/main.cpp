ba1dbaaf26606fd1798eab06a9904ebac39edead
AP\Adam <40206116@student.napier.ac.uk>
2019-01-24 12:10:54 +0000

    Pong Complete

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 const Keyboard::Key controls[4] = {
 	Keyboard::A,   // Player1 UP
 	Keyboard::D,   // Player1 Down
-	Keyboard::Up,  // Player2 UP
-	Keyboard::Down // Player2 Down
+	Keyboard::Left,  // Player2 UP
+	Keyboard::Right // Player2 Down
 };
 const Vector2f paddleSize(25.f, 100.f);
 const float ballRadius = 10.f;
@@ -16,11 +16,20 @@ const int gameHeight = 600;
 const float paddleSpeed = 400.f;
 
 Vector2f ballVelocity;
-bool server = true;
+bool server = false;
 
 CircleShape ball;
 RectangleShape paddles[2];
-
+void Reset()
+{
+	// reset paddle position
+	paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
+	paddles[1].setPosition(gameWidth - paddleSize.x, gameHeight / 2);
+	// reset Ball Position
+	ball.setPosition(gameWidth / 2, gameHeight / 2);
+	//ball velocity
+	ballVelocity = { (server ? 100.0f : -100.0f), 60.0f };
+}
 void Load() {
 	// Set size and origin of paddles
 	for (auto &p : paddles) {
@@ -30,16 +39,12 @@ void Load() {
 	// Set size and origin of ball
 	ball.setRadius(ballRadius - 3);
 	ball.setOrigin(ballRadius / 2, ballRadius / 2);
-	// reset paddle position
-	paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
-	paddles[1].setPosition(gameWidth-paddleSize.x,gameHeight/2);
-	// reset Ball Position
-	ball.setPosition(gameWidth/2,gameHeight/2);
-	//ball velocity
-	ballVelocity = { (server ? 100.0f : -100.0f), -60.0f };
+	Reset();
+	
 }
 
-void Update(RenderWindow &window) {
+void Update(RenderWindow &window) 
+{
 	// Reset clock, recalculate deltatime
 	static Clock clock;
 	float dt = clock.restart().asSeconds();
@@ -58,16 +63,69 @@ void Update(RenderWindow &window) {
 	}
 
 	// handle paddle movement
-	float direction = 0.0f;
-	if (Keyboard::isKeyPressed(controls[0])) {
-		direction--;
+	float p1Direction = 0.0f;
+	float p2Direction = 0.0f;
+	if (Keyboard::isKeyPressed(controls[0]) && paddles[0].getPosition().y > paddleSize.y*0.5) 
+	{
+		p1Direction--;
 	}
-	if (Keyboard::isKeyPressed(controls[1])) {
-		direction++;
+	if (Keyboard::isKeyPressed(controls[1]) && paddles[0].getPosition().y < gameHeight - paddleSize.y*0.5) {
+		p1Direction++;
+	} 
+	if (Keyboard::isKeyPressed(controls[2]) && paddles[1].getPosition().y > paddleSize.y*0.5) 
+	{
+		p2Direction--;
+	} 
+	if (Keyboard::isKeyPressed(controls[3]) && paddles[1].getPosition().y < gameHeight - paddleSize.y*0.5) {
+		p2Direction++;
 	}
-	paddles[0].move(0, direction * paddleSpeed * dt);
+	paddles[0].move(0, p1Direction * paddleSpeed * dt);
+	paddles[1].move(0, p2Direction * paddleSpeed * dt);
 	//move ball
 	ball.move(ballVelocity * dt);
+
+	// check ball collision
+	const float bx = ball.getPosition().x;
+	const float by = ball.getPosition().y;
+	if (by > gameHeight) { //bottom wall
+						   // bottom wall
+		ballVelocity.x *= 1.1f;
+		ballVelocity.y *= -1.1f;
+		ball.move(0, -10);
+	}
+	else if (by < 0) { //top wall
+					   // top wall
+		ballVelocity.x *= 1.1f;
+		ballVelocity.y *= -1.1f;
+		ball.move(0, 10);
+	}
+	else if (bx > gameWidth) {
+		// right wall
+		server = true;
+		Reset();
+	}
+	else if (bx < 0) {
+		// left wall
+		server = false;
+		Reset();
+	}
+	else if (
+		//ball is inline or behind paddle
+		bx < 10+paddleSize.x && by > paddles[0].getPosition().y - (paddleSize.y * 0.5) && by < paddles[0].getPosition().y + (paddleSize.y * 0.5)
+		) 
+	{
+		// bounce off left paddle
+		ballVelocity.x *= -1.1f;
+		ballVelocity.y *=  1.1f;
+		ball.move(0, 10);
+	}
+	else if (bx > (gameWidth-paddleSize.x-10) && by > paddles[1].getPosition().y - (paddleSize.y * 0.5) && by < paddles[1].getPosition().y + (paddleSize.y * 0.5))
+	{
+		// bounce off right paddle
+		ballVelocity.x *= -1.1f;
+		ballVelocity.y *=  1.1f;
+		ball.move(0, 10); 
+	}
 }
 
 void Render(RenderWindow &window) {
