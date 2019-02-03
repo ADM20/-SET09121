0437234e99b61515f51d1cfeee1431fdcba57b29
AP\Adam <40206116@student.napier.ac.uk>
2019-02-03 21:56:04 +0000

    libs wont work

--- practical_3/main.cpp
+++ practical_3/main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "levelsystem.h"
 //headers
 #include "entity.h"
 #include "player.h"
@@ -16,6 +17,14 @@ void Load()
 	auto player = new Player();
 	entity.push_back(player);
 
+	ls::loadLevelFile("res/maze_2.txt");
+	// Print the level to the console
+	for (size_t y = 0; y < ls::getHeight(); ++y) {
+		for (size_t x = 0; x < ls::getWidth(); ++x) {
+			cout << ls::getTile({ x, y });
+		}
+		cout << endl;
+	}
 }
 
 void Update(RenderWindow &window)
@@ -47,7 +56,7 @@ void Update(RenderWindow &window)
 void Render(RenderWindow &window)
 {
 	// Draw Everything
-
+	ls::render(window);
 	for (const auto e : entity)
 	{
 		e->render(window);
