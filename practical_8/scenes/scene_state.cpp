8f229f280be25b12176c706744d8e8a5c5e7ee77
ADM20 <40206116@live.napier.ac.uk>
2019-03-14 17:31:40 +0000

    Scenes Load

---
+++ practical_8/scenes/scene_state.cpp
@@ -0,0 +1,22 @@
+#include "scene_state.h"
+#include "../components/cmp_sprite.h"
+#include "../components/cmp_basic_movement.h"
+#include <LevelSystem.h>
+
+using namespace std;
+using namespace std::chrono;
+using namespace sf;
+
+void StateScene::Load(){}
+
+void StateScene::UnLoad(){
+    Scene::UnLoad();
+}
+
+void StateScene::Update(const double &dt){
+    Scene::Update(dt);
+}
+
+void StateScene::Render(){
+    Scene::Render();
+}
