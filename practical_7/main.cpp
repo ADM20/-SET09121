c18f50ae6f875886b87438484150d8cf90eef05d
ADM20 <40206116@live.napier.ac.uk>
2019-03-14 14:36:21 +0000

    Practical 7 Done

--- practical_7/main.cpp
+++ practical_7/main.cpp
@@ -9,6 +9,7 @@ Level1Scene level1;
 Level2Scene level2;
 Level3Scene level3;
 SteeringScene steeringScene;
+PathfindingScene pathfindingScene;
 
 int main() {
   Engine::Start(1920, 1080, "Platformer",&menu);
